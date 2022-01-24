ee43ee795839f926b7c47b0b4439a9051c997dff
guilherme-vm <73602048+guilherme-vm@users.noreply.github.com>
2022-01-24 17:53:34 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
+// When ready...
+window.addEventListener("load",function() {
+	// Set a timeout...
+	setTimeout(function(){
+		// Hide the address bar!
+		window.scrollTo(0, 1);
+	}, 0);
+});
+
+
+
 console.log("hellooo world");
 
 // open Menu for Mobile
